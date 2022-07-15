5952171dc023c78a3810af440a47067bf16dea59
Taylor Robie <taylorrobie@fb.com>
2022-07-15 09:33:13 -0700

    Optimize ThreadLocalDebugInfo lookup
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81453
    
    The way we do ThreadLocalDebugInfo lookup is to walk along a chain of shared_ptrs. This isn't particularly efficient since we expect reads to overwhelming dominate the API usage. (Push an observer, run millions of ops, pop the observer.)
    
    This PR introduces a very simple lookup table so that `ThreadLocalDebugInfo::get` becomes trivial. The impact on the other methods is minimal; the simply do the chain walk that `get` was doing. This saves another ~4% on `empty`.
    
    The one case where this may increase overhead is thread migration. (e.g. jit fork) I'm not particularly concerned since rebuilding the lookup table is cheap. (In general `debug_info` should not be deep.) And if we find that that's not the case, we can simply migrate the lookup table along with the TLS.
    ghstack-source-id: 161632773
    
    Differential Revision: [D37842246](https://our.internmc.facebook.com/intern/diff/D37842246/)

--- c10/util/ThreadLocalDebugInfo.cpp
+++ c10/util/ThreadLocalDebugInfo.cpp
@@ -1,3 +1,4 @@
+#include <c10/util/Exception.h>
 #include <c10/util/ThreadLocal.h>
 #include <c10/util/ThreadLocalDebugInfo.h>
 
@@ -6,16 +7,26 @@ namespace c10 {
 C10_DEFINE_TLS_static(std::shared_ptr<ThreadLocalDebugInfo>, tls_debug_info);
 #define debug_info (tls_debug_info.get())
 
+// Thread local values are default initialized. However we must manually zero
+// the lookup cache storage when using `c10::ThreadLocal`.
+#if defined(C10_PREFER_CUSTOM_THREAD_LOCAL_STORAGE)
+
+struct Cache {
+  Cache() { storage.fill(nullptr); }
+  ThreadLocalDebugInfo::lookup_cache_t storage_;
+};
+static ::c10::ThreadLocal<Cache> tls_lookup_cache
+#define lookup_cache (tls_lookup_cache->storage_)
+
+#else // defined(C10_PREFER_CUSTOM_THREAD_LOCAL_STORAGE)
+static thread_local ThreadLocalDebugInfo::lookup_cache_t lookup_cache;
+#endif // defined(C10_PREFER_CUSTOM_THREAD_LOCAL_STORAGE)
+
 /* static */
 DebugInfoBase* ThreadLocalDebugInfo::get(DebugInfoKind kind) {
-  ThreadLocalDebugInfo* cur = debug_info.get();
-  while (cur) {
-    if (cur->kind_ == kind) {
-      return cur->info_.get();
-    }
-    cur = cur->parent_info_.get();
-  }
-  return nullptr;
+  const auto index = static_cast<size_t>(kind);
+  TORCH_INTERNAL_ASSERT_DEBUG_ONLY(index < NUM_DEBUG_INFO_KINDS);
+  return lookup_cache[index];
 }
 
 /* static */
@@ -27,6 +38,7 @@ std::shared_ptr<ThreadLocalDebugInfo> ThreadLocalDebugInfo::current() {
 void ThreadLocalDebugInfo::_forceCurrentDebugInfo(
     const std::shared_ptr<ThreadLocalDebugInfo>& info) {
   debug_info = info;
+  lookup_cache = ThreadLocalDebugInfo::_make_lookup_cache();
 }
 
 /* static */
@@ -38,6 +50,7 @@ void ThreadLocalDebugInfo::_push(
   debug_info->parent_info_ = prev_info;
   debug_info->kind_ = kind;
   debug_info->info_ = info;
+  lookup_cache[static_cast<size_t>(kind)] = info.get();
 }
 
 /* static */
@@ -48,6 +61,7 @@ std::shared_ptr<DebugInfoBase> ThreadLocalDebugInfo::_pop(DebugInfoKind kind) {
       (size_t)kind);
   auto res = debug_info;
   debug_info = debug_info->parent_info_;
+  lookup_cache = ThreadLocalDebugInfo::_make_lookup_cache();
   return res->info_;
 }
 
@@ -58,6 +72,23 @@ std::shared_ptr<DebugInfoBase> ThreadLocalDebugInfo::_peek(DebugInfoKind kind) {
       "Expected debug info of type ",
       (size_t)kind);
   return debug_info->info_;
+}
+
+/* static */
+ThreadLocalDebugInfo::lookup_cache_t ThreadLocalDebugInfo::
+    _make_lookup_cache() {
+  ThreadLocalDebugInfo* cur = debug_info.get();
+
+  lookup_cache_t out;
+  out.fill(nullptr);
+  while (cur) {
+    const auto index = static_cast<size_t>(cur->kind_);
+    if (out[index] == nullptr) {
+      out[index] = cur->info_.get();
+    }
+    cur = cur->parent_info_.get();
+  }
+  return out;
 }
 
 DebugInfoGuard::DebugInfoGuard(
