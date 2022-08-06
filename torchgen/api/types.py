13d3f2e0b5dfe542cf6927b88975a700cc67185c
Peter Bell <peterbell10@live.co.uk>
2022-08-06 02:18:42 +0100

    Add memory_format overload for full, ones, zeros
    ghstack-source-id: 7fe70d5efa38905e46356ef5878f2fd16ddcbe59
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81379

--- torchgen/api/types.py
+++ torchgen/api/types.py
@@ -592,7 +592,14 @@ class NativeSignature:
     # The schema this signature is derived from
     func: FunctionSchema
 
+    cpp_no_default_args: Set[str]
     prefix: str = ""
+
+    @classmethod
+    def from_function(cls, func: NativeFunction, prefix: str = "") -> "NativeSignature":
+        return NativeSignature(
+            func=func.func, cpp_no_default_args=func.cpp_no_default_args, prefix=prefix
+        )
 
     def name(self) -> str:
         return self.prefix + native.name(self.func)
@@ -615,7 +622,7 @@ class NativeSignature:
         return f"{native.returns_type(self.func.returns).cpp_type()} (*)({args_str})"
 
     def arguments(self) -> List[Binding]:
-        return native.arguments(self.func)
+        return native.arguments(self.func, cpp_no_default_args=self.cpp_no_default_args)
 
     def returns_type(self) -> CType:
         return native.returns_type(self.func.returns)
@@ -746,7 +753,7 @@ def kernel_signature(
     if backend_index.external:
         return DispatcherSignature.from_schema(f.func, prefix=prefix)
     else:
-        return NativeSignature(f.func, prefix)
+        return NativeSignature.from_function(f, prefix=prefix)
 
 
 # Functions only, no types
