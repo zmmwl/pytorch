71594c237521bdc6430bea416ae87185ba60f62d
Andrew Gu <andgu@fb.com>
2022-10-29 14:01:35 +0000

    [FSDP()][3/N] Refactor `BackwardPrefetch` enum
    ghstack-source-id: 6dc3b4601e555b64cc78e5f1655ec74ff967c157
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/87917

---
+++ torch/distributed/fsdp/api.py
@@ -0,0 +1,34 @@
+"""
+This file includes public APIs for FSDP such as the classes used for the
+constructor arguments.
+"""
+
+from enum import auto, Enum
+
+
+class BackwardPrefetch(Enum):
+    """
+    This configures explicit backward prefetching, which can improve throughput
+    but may slightly increase peak memory usage.
+
+    For NCCL backend, any collectives, even if issued in different streams,
+    contend for the same per-device NCCL stream, which is why the relative
+    order in which the collectives are issued matters for overlapping. The
+    different backward prefetching settings correspond to different orderings.
+
+    - ``BACKWARD_PRE``: This prefetches the next set of parameters before the
+    current set of parameter's gradient computation. This improves backward
+    pass throughput by overlapping communication (next all-gather) and
+    computation (current gradient computation).
+    - ``BACKWARD_POST``: This prefetches the next set of parameters after the
+    current set of parameter's gradient computation. This may improve backward
+    pass throughput by overlapping communication (current reduce-scatter) and
+    computation (next gradient computation). Specifically, the next all-gather
+    is reordered to be before the current reduce-scatter.
+    """
+
+    # NOTE: For both modes, the ordering that defines "current" and "next" is
+    # not always correct in the current implementation, so this may cause some
+    # performance regression for some models.
+    BACKWARD_PRE = auto()
+    BACKWARD_POST = auto()
