db4a2c696b8d504a58d0b358c17c483476039a0e
Edward Z. Yang <ezyang@fb.com>
2022-11-09 08:36:08 -0500

    Meta registration for scatter
    To get the 'scatter' OpInfo working I had to complete out the meta
    registrations for all the API.  Doing inplace meta registrations is
    a bit annoying and it would be nice if we had a better way of doing
    it (maybe by setting out=self; which is /almost/ the way structured
    kernels do it.)
    
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    ghstack-source-id: 4a7281d005910286a409fa4ce3c5e33ce0a3a639
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88595

--- torch/_meta_registrations.py
+++ torch/_meta_registrations.py
@@ -1666,8 +1666,7 @@ def scatter_meta_impl(self, dim, index, src=None, reduce_=None, use_new_options=
 
 @register_meta(aten.scatter_add.default)
 def meta_scatter_add(self, dim, index, src):
-    scatter_meta_impl(self, dim, index, src, "add")
-    return self.new_empty(self.shape)
+    return scatter_meta_impl(self, dim, index, src, "add")
 
 
 @register_meta(aten.scatter_add_)
@@ -1676,14 +1675,16 @@ def meta_scatter_add_(self, dim, index, src):
     return self
 
 
+# NB: this hits scatter.value, scatter.src, scatter.value_reduce, scatter.reduce
+# scatter.value_out, scatter.src_out, scatter.value_reduce_out, scatter.reduce_out
 @register_meta(aten.scatter)
 @out_wrapper()
 def meta_scatter(self, dim, index, src_or_value, reduce=None):
     src = src_or_value if isinstance(src_or_value, torch.Tensor) else None
-    scatter_meta_impl(self, dim, index, src, reduce)
-    return self.new_empty(self.shape)
+    return scatter_meta_impl(self, dim, index, src, reduce)
 
 
+# NB: this hits scatter_.value, scatter_.src, scatter_.value_reduce, scatter_.reduce
 @register_meta(aten.scatter_)
 def meta_scatter_(self, dim, index, src_or_value, reduce=None):
     src = src_or_value if isinstance(src_or_value, torch.Tensor) else None
@@ -1694,8 +1695,7 @@ def meta_scatter_(self, dim, index, src_or_value, reduce=None):
 @register_meta([aten.scatter_reduce.two, aten.scatter_reduce.two_out])
 @out_wrapper()
 def meta_scatter_reduce_two(self, dim, index, src, reduce, include_self=True):
-    scatter_meta_impl(self, dim, index, src, reduce, use_new_options=True)
-    return self.new_empty(self.shape)
+    return scatter_meta_impl(self, dim, index, src, reduce, use_new_options=True)
 
 
 @register_meta(aten.scatter_reduce_.two)
