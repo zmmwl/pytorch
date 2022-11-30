68e9a376de1db85fad1fd014956485a5302860f1
anjali411 <chourdiaanjali123@gmail.com>
2022-11-30 20:31:09 +0000

    Run meta tests for tensor constructors
    ghstack-source-id: 28dccb40c5ca39ad57d8a204b31953df8056afc2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89911

--- test/test_meta.py
+++ test/test_meta.py
@@ -469,13 +469,16 @@ def run_meta_crossref(
         # often skipped as well).
         raise unittest.SkipTest("Original OpInfo is broken")
 
+    # This is needed to ensure that we run meta tests for constructors, since Meta Converter above
+    # won't get any hits for non _like constructors that don't take any tensor args
+    is_tensor_constructor = to_meta.no_hits_or_miss() and isinstance(rs, torch.Tensor)
 
     # TODO: also handle cases where func raise an exception
 
     # For now, only attempt if we managed to convert all tensor types
     # (if any of them failed, we're in a mixed device situation and
     # this isn't well supported)
-    if do_meta and to_meta.successful():
+    if do_meta and (to_meta.successful() or is_tensor_constructor):
         # Special cases
         if func is torch.tensor_split:
             # Use original indices_or_sections, this argument is data dependent
@@ -503,7 +506,7 @@ def run_meta_crossref(
                     indices.append(meta_index)
             meta_args = (meta_args[0], indices)
 
-        if kwargs.get("device", None) is not None:
+        if kwargs.get("device", None) is not None or is_tensor_constructor:
             meta_kwargs["device"] = "meta"
 
         try:
