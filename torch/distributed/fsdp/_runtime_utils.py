e9c9c00bd2678348f34b7f7e8e8f4f5358a6ab53
Andrew Gu <andgu@fb.com>
2022-12-14 22:39:47 +0000

    [FSDP][1/N] Add `_get_fsdp_states()`
    ghstack-source-id: 5d287f14e93ac565464e87d9337bf921405ea8d2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90860

--- torch/distributed/fsdp/_runtime_utils.py
+++ torch/distributed/fsdp/_runtime_utils.py
@@ -1,6 +1,16 @@
 import functools
 import warnings
-from typing import Any, Callable, Dict, Iterable, List, no_type_check, Optional, Tuple
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    no_type_check,
+    Optional,
+    Set,
+    Tuple,
+)
 
 import torch
 import torch.nn as nn
@@ -11,6 +21,7 @@ from torch.distributed.fsdp._common_utils import (
     _all_handles,
     _assert_in_training_states,
     _FSDPState,
+    _get_module_fsdp_state,
     _is_composable,
     TrainingState,
 )
