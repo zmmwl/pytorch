679fb73df07ee61d548a7a8da66d4cf7262af04a
Peter Bell <peterbell10@live.co.uk>
2023-01-03 01:39:24 +0000

    squeeze: allow squeezing multiple dimensions at once
    Ref #70924
    
    This addresses part 1 of the issue, allowing `torch.squeeze` to be
    passed a tuple of dimensions. e.g.
    ```python
    x.squeeze(0).squeeze(0)
    ```
    can now be written
    ```python
    x.squeeze((0, 1))
    ```
    (assuming x has at least 2 dimensions)
    
    ghstack-source-id: 4309a30de06b2de4be94518f039695ec11b58f0d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/89017

--- aten/src/ATen/FunctionalInverses.cpp
+++ aten/src/ATen/FunctionalInverses.cpp
@@ -3,6 +3,7 @@
 
 #include <ATen/ATen.h>
 #include <ATen/ExpandUtils.h>
+#include <ATen/WrapDimUtilsMulti.h>
 
 #include <utility>
 namespace at {
@@ -54,6 +55,26 @@ Tensor unsqueeze_copy_to(const Tensor & self, int64_t dim, c10::SymIntArrayRef s
     }
   }
   return self;
+}
+
+Tensor unsqueeze_copy_to(const Tensor & self, IntArrayRef dim, IntArrayRef sizes, bool reapply_views) {
+  const auto ndim = sizes.size();
+  const auto mask = at::dim_list_to_bitset(dim, ndim);
+  if (ndim == 0) {
+    return self;
+  }
+
+  Tensor result = self;
+  for (const auto d : c10::irange(ndim)) {
+    if (mask.test(d) && sizes[d] == 1) {
+      if (reapply_views) {
+        result = at::unsqueeze(result, d);
+      } else {
+        result = at::unsqueeze_copy(result, d);
+      }
+    }
+  }
+  return result;
 }
 
 // Note [Functionalization Pass: View Inverses].
@@ -213,6 +234,10 @@ Tensor FunctionalInverses::squeeze_copy_inverse(const Tensor& base, const Tensor
 
 Tensor FunctionalInverses::squeeze_copy_dim_inverse(const Tensor& base, const Tensor& mutated_view, bool reapply_views, int64_t dim) {
     return unsqueeze_copy_to(mutated_view, dim, base.sym_sizes(), reapply_views);
+}
+
+Tensor FunctionalInverses::squeeze_copy_dims_inverse(const Tensor& base, const Tensor& mutated_view, bool reapply_views, IntArrayRef dim) {
+  return unsqueeze_copy_to(mutated_view, dim, base.sizes(), reapply_views);
 }
 
 Tensor FunctionalInverses::t_copy_inverse(const Tensor& base, const Tensor& mutated_view, bool reapply_views) {
