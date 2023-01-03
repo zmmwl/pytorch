85b4131467219b6b260f89cb0f49c3929b18db3b
samdow <samdow@fb.com>
2023-01-03 15:26:21 -0500

    [modes] fix modes so not implemented falls through to the next mode, not to subclass
    ghstack-source-id: f9faa1eaf8afaddbc101e4ec138d2cd0a199f506
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/91521

--- test/test_python_dispatch.py
+++ test/test_python_dispatch.py
@@ -1044,10 +1044,15 @@ $3 = torch._ops.aten.add.Tensor($1, $2)""")
         self.assertEqual(mode.post_count, 1)
         self.assertEqual(sub_count, 1)
 
-        # make sure this doesn't error
-        with PoliteMode():
-            with PoliteMode():
+
+        sub_count = 0
+        with PoliteMode() as mode1:
+            with PoliteMode() as mode2:
                 a.abs()
+
+        self.assertEqual(mode1.pre_count, 2)
+        self.assertEqual(mode2.pre_count, 2)
+        self.assertEqual(sub_count, 1)
 
     def test_nesting_same_mode(self):
         # If the pushed mode is the same instance as the current mode, we allow pushing an already active mode.
