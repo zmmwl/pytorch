140c3c1b48c298ae578afcb2803144ecefe7ae42
Nikita Karetnikov <nikita@karetnikov.org>
2023-01-24 03:04:21 +0100

    [inductor] skip CUDA tests under ASAN
    ghstack-source-id: 76053b5b116384ebd050368356ea642792153e78
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/92883

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -5887,7 +5887,7 @@ if HAS_CPU:
                             assert metrics.generated_cpp_vec_kernel_count == 1
 
 
-if HAS_CUDA:
+if HAS_CUDA and not TEST_WITH_ASAN:
     import triton
     import triton.language as tl
 
@@ -6642,7 +6642,7 @@ class ExprPrinterTests(TestCase):
             self.assertEqual(texpr(expr), result)
 
 
-if HAS_CUDA:
+if HAS_CUDA and not TEST_WITH_ASAN:
 
     class RNNTest(TestCase):
         class Model(torch.nn.Module):
