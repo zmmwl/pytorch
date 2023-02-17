b01176494044209aa2cfc3a7e92bdb96dd1beb50
Bin Bao <binbao@fb.com>
2023-02-17 18:03:58 +0000

    [Reland] Set torch.backends.cudnn.enabled to false when testing accuracy
    Summary: This is the reland of #94363.
    
    ghstack-source-id: 218ebd8538f9ec6b2620f97d2180a5144572d925
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94712

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -104,6 +104,7 @@ CI_SKIP[CI("aot_eager", training=True)] = [
     "resnet50_quantized_qat",  # fp64_OOM
     "moco",
     "pytorch_struct",
+    "pytorch_unet",  # fp64_OOM
     "vision_maskrcnn",
     # Huggingface
     "MBartForConditionalGeneration",  # OOM
@@ -112,13 +113,8 @@ CI_SKIP[CI("aot_eager", training=True)] = [
     # TIMM
     "cait_m36_384",  # fp64_OOM
     "convit_base",  # fp64_OOM
-    "fbnetv3_b",  # Accuracy (blocks.2.2.bn1.weight.grad)
-    "levit_128",  # Accuracy (patch_embed.0.c.weight.grad)
-    "sebotnet33ts_256",  # Accuracy (stem.conv1.conv.weight.grad)
-    "xcit_large_24_p8_224",  # fp64_OOM,
-    "gernet_l",  # accuracy https://github.com/pytorch/pytorch/issues/93847
-    "gluon_xception65",  # accuracy https://github.com/pytorch/pytorch/issues/93847
-    "tinynet_a",  # accuracy https://github.com/pytorch/pytorch/issues/93847
+    "sebotnet33ts_256",  # Accuracy (stages.1.1.attn.fc1.bias.grad)
+    "xcit_large_24_p8_224",  # fp64_OOM
 ]
 
 CI_SKIP[CI("inductor", training=False)] = [
@@ -134,6 +130,8 @@ CI_SKIP[CI("inductor", training=False)] = [
     "pytorch_struct",  # Test eval is not implemented
     "pyhpc_equation_of_state",  # Accuracy
     "pyhpc_turbulent_kinetic_energy",  # Accuracy
+    "pytorch_unet",  # OOM
+    "squeezenet1_1",  # accuracy
     "tacotron2",
     "vision_maskrcnn",  # accuracy
     # Huggingface
@@ -142,8 +140,6 @@ CI_SKIP[CI("inductor", training=False)] = [
     "OPTForCausalLM",  # OOM
     # TIMM
     "cait_m36_384",  # Accuracy
-    "botnet26t_256",  # accuracy https://github.com/pytorch/pytorch/issues/93847
-    "gluon_xception65",  # accuracy https://github.com/pytorch/pytorch/issues/93847
 ]
 
 CI_SKIP[CI("inductor", training=True)] = [
@@ -151,8 +147,8 @@ CI_SKIP[CI("inductor", training=True)] = [
     # TorchBench
     "Background_Matting",  # fp64_OOM
     "dlrm",  # Fails on CI - unable to repro locally
+    "functorch_maml_omniglot",  # accuracy - unable to repro locally
     "hf_T5_base",  # accuracy
-    "mobilenet_v3_large",  # accuracy
     "resnet50_quantized_qat",  # Eager model failed to run
     # Huggingface
     "BlenderbotForCausalLM",  # OOM
@@ -164,8 +160,11 @@ CI_SKIP[CI("inductor", training=True)] = [
     # TIMM
     "convit_base",  # fp64_OOM
     "eca_halonext26ts",  # accuracy
-    "fbnetv3_b",  # accuracy
+    "fbnetv3_b",  # accuracy - unable to repro locally
+    "gernet_l",  # Accuracy failed for key name stages.3.0.conv3_1x1.bn.running_var
+    "gluon_xception65",  # Accuracy failed for key name mid.block7.rep.conv1.bn.weight.grad
     "levit_128",  # fp64_OOM
+    "resnest101e",  # Accuracy failed for key name layer1.2.conv2.bn1.running_var
     # https://github.com/pytorch/pytorch/issues/94066
     "sebotnet33ts_256",  # Accuracy failed for key name stem.conv1.conv.weight.grad
     "xcit_large_24_p8_224",  # fp64_OOM
@@ -1909,7 +1908,9 @@ def run(runner, args, original_dir=None):
             # TODO - Using train mode for timm_models. Move to train mode for HF and Torchbench as well.
             args.use_eval_mode = True
         inductor_config.fallback_random = True
-        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.enabled = False
+        # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
+        os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
 
         # Remove randomeness when torch manual seed is called
         patch_torch_manual_seed()
