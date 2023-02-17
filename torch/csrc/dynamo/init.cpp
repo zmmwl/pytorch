5b868206ee95695b69b7a3b1a9de247a854fca19
Michael Voznesensky <voznesenskym@gmail.com>
2023-02-17 02:06:24 +0000

    [RFC] [Needs testing and sweep] Remove C++ guards
    ghstack-source-id: 8ed68b46ccf6b9f9820108ae4e81316215839d7c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94973
    
    rm more
    
    Wip

--- torch/csrc/dynamo/init.cpp
+++ torch/csrc/dynamo/init.cpp
@@ -2,7 +2,6 @@
 
 #include <torch/csrc/Exceptions.h>
 #include <torch/csrc/dynamo/eval_frame.h>
-#include <torch/csrc/dynamo/guards.h>
 
 static struct PyModuleDef _module =
     {PyModuleDef_HEAD_INIT, "torch._C._dynamo", "", -1, NULL};
@@ -19,11 +18,6 @@ void initDynamoBindings(PyObject* torch) {
   PyObject* eval_frame = torch_c_dynamo_eval_frame_init();
   if (eval_frame == NULL ||
       PyModule_AddObject(dynamo, "eval_frame", eval_frame) != 0) {
-    throw python_error();
-  }
-
-  PyObject* guards = torch_c_dynamo_guards_init();
-  if (guards == NULL || PyModule_AddObject(dynamo, "guards", guards) != 0) {
     throw python_error();
   }
 }
