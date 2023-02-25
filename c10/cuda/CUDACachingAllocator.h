d816ecac8196a249bdea3d6a401cc388d0d32cac
Elias Ellison <elias.ellison@gmail.com>
2023-02-25 03:08:25 +0000

    Checkpoint CUDA Allocator Private Pool State
    ghstack-source-id: 74aa31b9ab4fc7d3315be3afd3b7133d0696576b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94653

--- c10/cuda/CUDACachingAllocator.h
+++ c10/cuda/CUDACachingAllocator.h
@@ -129,7 +129,12 @@ struct SegmentInfo {
   int64_t active_size = 0;
   cudaStream_t stream = 0;
   bool is_large = false;
+  MempoolId_t owner_private_pool_id = {0, 0};
   std::vector<BlockInfo> blocks;
+};
+
+struct AllocatorState {
+  virtual ~AllocatorState() = default;
 };
 
 struct TraceEntry {
@@ -212,6 +217,12 @@ class CUDAAllocator : public Allocator {
       bool alloc_trace_record_context) = 0;
   virtual void attachOutOfMemoryObserver(OutOfMemoryObserver observer) = 0;
   virtual bool needsPoolSpecificPeerAccess() = 0;
+  virtual std::shared_ptr<AllocatorState> getCheckpointState(
+      int device,
+      MempoolId_t id) = 0;
+  virtual void setCheckpointPoolState(
+      int device,
+      std::shared_ptr<AllocatorState> pps) = 0;
   virtual std::string name() = 0;
 };
 
@@ -277,6 +288,18 @@ inline void resetPeakStats(int device) {
 
 inline SnapshotInfo snapshot() {
   return get()->snapshot();
+}
+
+inline std::shared_ptr<AllocatorState> getCheckpointState(
+    int device,
+    MempoolId_t id) {
+  return get()->getCheckpointState(device, id);
+}
+
+inline void setCheckpointPoolState(
+    int device,
+    std::shared_ptr<AllocatorState> pps) {
+  return get()->setCheckpointPoolState(device, pps);
 }
 
 // CUDAGraph interactions
