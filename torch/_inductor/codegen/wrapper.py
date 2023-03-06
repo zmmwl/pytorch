08eb4bc310ca15ad27b698384615228b8d7ffa5e
Zachary DeVito <zdevito@gmail.com>
2023-03-06 21:01:04 +0000

    [memory debugging] Extract frame information from inductor
    ghstack-source-id: 657583c74bdd5a7646b1799e0deca286222fe806
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/95753

--- torch/_inductor/codegen/wrapper.py
+++ torch/_inductor/codegen/wrapper.py
@@ -12,7 +12,7 @@ from torch._dynamo.utils import dynamo_timed
 
 from .. import codecache, config, ir
 from ..codecache import code_hash, cpp_compile_command, get_code_path
-from ..utils import cache_on_self, has_triton, sympy_dot, sympy_product
+from ..utils import cache_on_self, has_triton, LineContext, sympy_dot, sympy_product
 from ..virtualized import V
 from .common import CodeGen, DeferredLine, IndentedBuffer, Kernel, PythonPrinter
 
@@ -515,6 +515,7 @@ class WrapperCodeGen(CodeGen):
 
             device_cm_stack = contextlib.ExitStack()
             for line in self.lines:
+
                 if isinstance(line, MemoryPlanningLine):
                     line.codegen(self.wrapper_call)
                 elif isinstance(line, EnterCudaDeviceContextManagerLine):
@@ -547,7 +548,7 @@ class WrapperCodeGen(CodeGen):
 
         self.add_benchmark_harness(result)
 
-        return result.getvalue()
+        return result.getvaluewithlinemap()
 
     def add_benchmark_harness(self, output):
         """
@@ -621,6 +622,9 @@ class WrapperCodeGen(CodeGen):
 
     def writeline(self, line):
         self.lines.append(line)
+
+    def enter_context(self, ctx):
+        self.lines.append(LineContext(ctx))
 
 
 class CppWrapperCodeGen(WrapperCodeGen):
